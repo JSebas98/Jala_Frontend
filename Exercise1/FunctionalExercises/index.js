e14112dbfd295ba2d3a734ce9dc4878cc71226ac
Sebastian Beltran <sebas.beltran.98@gmail.com>
2022-04-27 14:41:19 -0500

    Solve exercise 1 problems

---
+++ Exercise1/FunctionalExercises/index.js
@@ -0,0 +1,102 @@
+/**
+ * Write a JavaScrip program to find the most frequent item
+ * of an array.
+ * Sample array: let arr 1 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]
+ * Sample output: a (5 times)
+ */
+
+// let arr1 = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]
+
+// function findMostFrequent(arr){
+//     let values = {};
+//     arr.map((val) => {
+//         return val in values ? values[val] += 1 : values[val] = 1;  
+//     });
+//     return Object.keys(values).
+//         reduce((prevKey, actKey) => {
+//             return values[prevKey] > values[actKey] ? `${prevKey}: ${values[prevKey]}` : `${actKey}: ${values[actKey]}`
+//         });
+// }
+
+// console.log(findMostFrequent(arr1));
+
+
+/**
+ * Write a JavaScript program to find the sum of squares of a numeric vector.
+ * Sample array: let arr1 = [0, 1, 2, 3, 4]
+ * Sample output: 30
+ */
+
+// let arr1 = [0, 1, 2, 3, 4]
+
+// function sumSquares(numbers) {
+//     return numbers.reduce((total, n) => {
+//         return total + n ** 2;
+//     });
+// }
+
+// console.log(sumSquares(arr1));
+
+
+/**
+ * Write a JavaScript function to remove 'null', '0', 'false', 'undefined', and 'NaN'
+ * values from an array.
+ * Sample array: let arr1 = [Nan, 0, 15, false, -22, '', undefined, 47, null]
+ * Expected result: [15, -22, 47]
+ */
+
+//  let arr1 = [NaN, 0, 15, false, -22, '', undefined, 47, null]
+
+//  function cleanArray(arr) {
+//     return arr.filter((elem) => elem);
+//  }
+
+//  console.log(cleanArray(arr1));
+
+/**
+ * Working with emojis
+ * Primal: '🐮', '🥔', '🐔', '🌽'
+ * Cooked: '🍔', '🍟', '🍿', '🍗'
+ * Status after eat: 🤤
+ */
+
+let primal = ['🐮', '🥔', '🐔', '🌽'];
+
+// Cook primal
+function cookPrimal(primal) {
+    return primal.map((elem) => {
+        if (elem == '🐮') {return '🍔'}
+        if (elem == '🥔') {return '🍟'}
+        if (elem == '🐔') {return '🍿' }
+        if (elem == '🌽') {return '🍗'}
+    });
+}
+
+// Filter vegetarian
+function getVegetarianFood(cookedFood) {
+    return cookedFood.filter((food) => {
+        if (food != '🍔' && food != '🍗') {return food}
+    });
+}
+
+// Filter food with meat
+function getFoodWithMeat(cookedFood) {
+    return cookedFood.filter((food) => {
+        if (food != '🍟' && food != '🍿') {return food}
+    });
+}
+
+// Eat cooked food
+function eatFood(cookedFood) {
+    return cookedFood.reduce((status, food) => {
+        status = '🤤' 
+        return food = status;
+    });
+}
+
+const cookedFood = cookPrimal(primal);
+
+console.log(cookedFood);
+console.log(getVegetarianFood(cookedFood));
+console.log(getFoodWithMeat(cookedFood));
+console.log(eatFood(cookedFood));
---
+++ Exercise1/TheCat/cat.js
@@ -0,0 +1,128 @@
+// Cat class
+class Cat {
+    #loneliness
+
+    constructor (hunger, loneliness, tiredness) {
+        this.hunger = this.#adjustTopValues(hunger);
+        this.loneliness = this.#setLonelinessLevel(loneliness);
+        this.tiredness = this.#adjustTopValues(tiredness);
+    }
+
+    /**
+     * Generates random number between 1 and 10 to indicate
+     * cat's level of satisfaction with task.
+     * @returns a random number between 1 and 10
+     */
+    #generateStatusValue(){
+        return Math.floor((Math.random() * 10) + 1);
+    }
+
+    #adjustTopValues(propertyValue){
+        if (propertyValue < 0) {
+            return 0;
+        }
+
+        if (propertyValue > 10) {
+            return 10;
+        }
+
+        return propertyValue;
+    }
+
+    #setLonelinessLevel(value) {
+        this.#loneliness = this.#adjustTopValues(value);
+    }
+
+    /**
+     * Prints in console the cat's current state
+     * @returns a String with cat's levels of hunger, loneliness, tiredness
+     */
+    getStatus () {
+        console.log(`\nCat status:
+        Hunger: ${this.hunger},
+        Loneliness: ${this.#loneliness},
+        Tiredness: ${this.tiredness}`);
+    }
+
+    getCatNeeds(){
+        if (this.hunger >= 8){
+            console.log("Cat needs to be fed urgently!");
+        }
+
+        if (this.#loneliness >= 8) {
+            console.log("Cat needs to play with you urgently!");
+        }
+
+        if (this.tiredness >= 8) {
+            console.log("Cat needs to sleep!");
+        }
+    }
+
+    getHungerLevel(){
+        return `Cat's hunger level: ${this.hunger}`;
+    }
+
+    getTirednessLevel(){
+        return `Cat's tiredness level: ${this.tiredness}`;
+    }
+
+    getLonelinessLevel(){
+        return `Cat's loneliness level: ${this.#loneliness}`;
+    }
+
+    feed() {
+        console.log("Cat is eating... Ñom, ñom...")
+        let satisfactionLevel = this.#generateStatusValue();
+        this.hunger = this.#adjustTopValues(this.hunger - satisfactionLevel);
+
+        console.log("Cat has been fed!")
+    }
+
+    sleep() {
+        console.log("Cat is sleeping... Zzzz...")
+        let satisfactionLevel = this.#generateStatusValue();
+        this.tiredness = this.#adjustTopValues(this.tiredness - satisfactionLevel);
+        
+        console.log("Cat is awake now!");
+    }
+
+    play () {
+        console.log("Cat is playing!")
+        let satisfactionLevel = this.#generateStatusValue();
+        let tirednessLevel = this.#generateStatusValue();
+        this.#setLonelinessLevel(this.#loneliness - satisfactionLevel);
+        this.tiredness = this.#adjustTopValues(this.tiredness + tirednessLevel);
+        
+        console.log("Cat does not want to play anymore!");
+    }
+
+    static feedCats(...Cats){
+        Cats.forEach((cat) => cat.feed());
+    }
+
+}
+
+
+let misifus = new Cat(10, 10, 10);
+let tommy = new Cat(8, 5, 3);
+let katty = new Cat(4, 9, 6);
+
+// misifus.feed();
+// misifus.getStatus();
+// misifus.sleep();
+// misifus.getStatus();
+console.log(misifus.getHungerLevel());
+console.log(misifus.getLonelinessLevel());
+console.log(misifus.getTirednessLevel());
+misifus.play();
+misifus.getStatus();
+
+
+// Cat.feedCats(misifus, tommy, katty);
+// misifus.getStatus();
+// tommy.getStatus();
+// katty.getStatus();
+
+// misifus.getCatNeeds();
+// tommy.getCatNeeds();
+// katty.getCatNeeds();

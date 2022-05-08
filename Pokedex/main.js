19fbc491d0872e2a16a68b497d401526af9758bd
Sebastian Beltran <sebas.beltran.98@gmail.com>
2022-05-08 13:50:05 -0500

    Creating poke-cards from Pokemon API information

--- Pokedex/main.js
+++ Pokedex/main.js
@@ -1,13 +1,40 @@
-dataPokemons.results.forEach((pokemon, i) => {
-    let container = document.getElementById("pokedex");
-    let pokeCard = document.createElement("poke-card");
-    let backgroundColor = pokemonColorMap[i+1];
+// Fetching from PokeAPI usinf API Fetch}
+const offset = 0;
 
-    pokeCard.setAttribute("p-image", getPokemonImageUri(i+1));
-    pokeCard.setAttribute("p-name", pokemon.name);
-    pokeCard.setAttribute("color", backgroundColor);
+fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`)
+    .then((resp) => resp.json()) // Convert into json
+    .then((data) => {
+        let pokemons = data.results;
+        pokemons.forEach((pokemon, i) => {
+            let container = document.getElementById("pokedex");
+            let pokeCard = document.createElement("poke-card");
+            let backgroundColor = pokemonColorMap[i+offset+1];
 
-    container.appendChild(pokeCard);
-});
+            pokeCard.setAttribute("p-image", getPokemonImageUri(i+offset+1));
+            pokeCard.setAttribute("p-name", pokemon.name);
+            pokeCard.setAttribute("color", backgroundColor);
 
-customElements.define('poke-card', PokemonCard);
+            container.appendChild(pokeCard);            
+        })
+
+        customElements.define('poke-card', PokemonCard);
+    }) 
+    .catch((error) => {
+        console.log(error);
+    });
+
+
+// Creating poke-cards from mockup information
+// dataPokemons.results.forEach((pokemon, i) => {
+//     let container = document.getElementById("pokedex");
+//     let pokeCard = document.createElement("poke-card");
+//     let backgroundColor = pokemonColorMap[i+1];
+
+//     pokeCard.setAttribute("p-image", getPokemonImageUri(i+1));
+//     pokeCard.setAttribute("p-name", pokemon.name);
+//     pokeCard.setAttribute("color", backgroundColor);
+
+//     container.appendChild(pokeCard);
+// });
+
+// customElements.define('poke-card', PokemonCard);
